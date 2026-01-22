c194fe7fc2d84e3a02250105b5f6557a8d8dfe55
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-01-22 16:55:03 +0530

    gsc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
         {/* AdSense Script */}
         <Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5188894530340921"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
