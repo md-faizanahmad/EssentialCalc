4fe55e446ebac8c101d98e36a7a6de7685286580
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-01-31 19:21:40 +0530

    tags manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/layouts/Header";
 import Footer from "@/components/layouts/Footer";
 import Breadcrumbs from "@/components/layouts/Breadcrumbs";
 import ScrollToTop from "@/components/layouts/ScrollToTop";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -74,7 +75,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NR3QH493');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} bg-white text-gray-900 antialiased`}>
+        {" "}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NR3QH493"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <div className="flex flex-col min-h-screen relative bg-grid">
           <Header />
           <Breadcrumbs />
